85844d39ed02584e1225e2c3aea9ba49ffc234b5
Vadzim Antonau <vanet@tut.by>
2021-11-21 12:58:33 +0300

    refact: change tests structure

--- test/checkArgs.test.js
+++ test/checkArgs.test.js
@@ -1,53 +1,38 @@
+const path = require("path");
 const { expect } = require("@jest/globals");
 const { isArgsOk, getValue } = require('../src/checkArgs.js');
-const { ArgRequiredError, ArgDuplicateError, IncorrectConfigError} = require('../src/errors.js');
+// const { ArgRequiredError, ArgDuplicateError, IncorrectConfigError} = require('../src/errors.js');
+
+beforeEach(() => {
+  process.argv = [process.execPath, path.resolve('./..', 'caesar-cli.js')]
+});
 
 describe('Caesar Cipher CLI checkArgs isArgsOk Tests', () => {
   test('should throw error Duplicated options are not allowed', () => {
-    //Input: User passes the same cli argument twice; Result: Error message is shown
-    process.argv = ['folder-path', 'file-path', '-c', 'C1-C1-A-R0', '--config', 'C0'];
-    try {
-      isArgsOk();
-    } catch (error) {
-      expect(error).toEqual(new ArgDuplicateError());
-    }
+    //1. Input: User passes the same cli argument twice; Result: Error message is shown
+    process.argv.push('-c', 'C1-C1-A-R0', '--config', 'C0');
+    expect(isArgsOk).toThrowError(/Duplicated options are not allowed/);
   });
 
   test('should throw error Missing required argument -c (--config)', () => {
-    //Input: User doesn't pass -c or --config argument; Result: Error message is shown
-    process.argv = ['folder-path', 'file-path'];
-    try {
-      isArgsOk();
-    } catch (error) {
-      expect(error).toEqual(new ArgRequiredError('-c (--config)'));
-    }
+    //2. Input: User doesn't pass -c or --config argument; Result: Error message is shown
+    expect(isArgsOk).toThrowError("Missing required argument: -c (--config)");
   });
 
   test('should throw error Incorrect config definition', () => {
-    process.argv = ['folder-path', 'file-path', '-c', 'C1-C1-A-R0-'];
-    try {
-      isArgsOk();
-    } catch (error) {
-      expect(error).toEqual(new IncorrectConfigError());
-    }
+    //5. Input: User passes incorrent symbols in argument for --config; Result: Error message is shown;
+    process.argv.push('-c', 'C1-C1-A-R0-');
+    expect(isArgsOk).toThrowError(/Incorrect config definition/);
   });
 });
 
 describe('Caesar Cipher CLI checkArgs getValue Tests', () => {
   test('should return C1-C1-A-R0', () => {
-    process.argv = ['folder-path', 'file-path', '-c', 'C1-C1-A-R0'];
-    try {
-      getValue('-c');
-    } catch (error) {
-      expect(error).toEqual('C1-C1-A-R0');
-    }
+    process.argv.push('-c', 'C1-C1-A-R0');
+    expect(getValue('-c')).toEqual('C1-C1-A-R0');
   });
   test('should return null', () => {
-    process.argv = ['folder-path', 'file-path', '-c', 'C1-C1-A-R0'];
-    try {
-      getValue('-i');
-    } catch (error) {
-      expect(error).toEqual(null);
-    }
+    process.argv.push('-c', 'C1-C1-A-R0');
+    expect(getValue('-i')).toEqual(null);
   });
 });
