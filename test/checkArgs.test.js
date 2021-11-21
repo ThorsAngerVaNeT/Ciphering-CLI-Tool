aa6089056f57d9c548a82434fcfe87b226aa7ddc
Vadzim Antonau <vanet@tut.by>
2021-11-21 19:50:51 +0300

    fix: moch function wrong behavior

--- test/checkArgs.test.js
+++ test/checkArgs.test.js
@@ -5,22 +5,6 @@ beforeEach(() => {
   process.argv = [process.execPath, path.resolve('./..', 'caesar-cli.js')]
 });
 
-test('should correct config with mocking function', () => {
-  process.argv.push('-c', 'C1-C1-A-R0-');
-  checkArgs.getValue = jest.fn((arg) => {
-    const flagIndex = process.argv.indexOf(arg);
-    let argVal = flagIndex !== -1 ? process.argv[flagIndex + 1] : null;
-    if(argVal.substring(argVal.length-1) == "-"){
-      argVal = argVal.slice(0, -1);
-      process.argv[flagIndex + 1] = argVal;
-    }
-    return argVal;
-  });
-  
-  expect(checkArgs.getValue('-c')).toBe('C1-C1-A-R0');
-  expect(checkArgs.isArgsOk()).toBe(true);
-});
-
 describe('Caesar Cipher CLI checkArgs Module', () => {
   describe('isArgsOk Unit Tests', () => {
     //There are unit tests, to check tests from task2 decription see caesar-cli.test.js
@@ -57,5 +41,23 @@ describe('Caesar Cipher CLI checkArgs Module', () => {
       process.argv.push('-c', 'C1-C1-A-R0');
       expect(checkArgs.getValue('-i')).toEqual(null);
     });
+  });
+});
+
+describe('Mock function implementation', () => {
+  test('should correct config with mocking function', () => {
+    process.argv.push('-c', 'C1-C1-A-R0-');
+    checkArgs.getValue = jest.fn((arg) => {
+      const flagIndex = process.argv.indexOf(arg);
+      let argVal = flagIndex !== -1 ? process.argv[flagIndex + 1] : null;
+      if(argVal.substring(argVal.length-1) == "-"){
+        argVal = argVal.slice(0, -1);
+        process.argv[flagIndex + 1] = argVal;
+      }
+      return argVal;
+    });
+    
+    expect(checkArgs.getValue('-c')).toBe('C1-C1-A-R0');
+    expect(checkArgs.isArgsOk()).toBe(true);
   });
 });
